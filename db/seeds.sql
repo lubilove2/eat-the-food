3c11b1bebbdbc462744debd3fa22107567b55e70
lubilove2 <lubaba_yemer@yahoo.com>
2017-05-17 04:45:09 -0400

    Update seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO burgers (burger_name) VALUES ('Rodeo Burger');
-INSERT INTO burgers (burger_name) VALUES ('Toro Burger');
-INSERT INTO burgers (burger_name) VALUES ('Brisket Burger');
-INSERT INTO burgers (burger_name) VALUES ('Texas Burger');
-INSERT INTO burgers (burger_name) VALUES ('Buffalo Burger');
+INSERT INTO burgers (burger_name) VALUES ('China burger');
+INSERT INTO burgers (burger_name) VALUES ('Japan burger');
+INSERT INTO burgers (burger_name) VALUES ('Homemade burger');
+INSERT INTO burgers (burger_name) VALUES ('Medium burger');
+INSERT INTO burgers (burger_name) VALUES ('Buffalo burger');
 INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Chicken Burger', true);
